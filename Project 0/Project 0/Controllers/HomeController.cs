05f15ee0d0a5c68c3edbd8f9e25725ea82e8e6bb
Harrison Hess <harrisonhess1@aol.com>
2020-08-07 19:42:03 -0400

    Created About Us page view

--- Project 0/Project 0/Controllers/HomeController.cs
+++ Project 0/Project 0/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Project_0.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
