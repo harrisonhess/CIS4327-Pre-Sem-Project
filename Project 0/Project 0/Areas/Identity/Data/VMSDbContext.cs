c1c0eec5987d5a333b518ac72f17720e4f1c45d7
Chloe Polder <cpolder27@gmail.com>
2020-08-10 18:09:34 -0400

    Adding Identity with login/register capability

---
+++ Project 0/Project 0/Areas/Identity/Data/VMSDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project_0.Areas.Identity.Data;
+
+namespace Project_0.Data
+{
+    public class VMSDbContext : IdentityDbContext<VMSUser>
+    {
+        public VMSDbContext(DbContextOptions<VMSDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Project 0/Project 0/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Project_0.Areas.Identity.Data;
+using Project_0.Data;
+
+[assembly: HostingStartup(typeof(Project_0.Areas.Identity.IdentityHostingStartup))]
+namespace Project_0.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<VMSDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("VMSDbContextConnection")));
+
+                services.AddDefaultIdentity<VMSUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<VMSDbContext>();
+            });
+        }
+    }
+}
