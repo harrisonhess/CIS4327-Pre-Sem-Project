78ab91a76665815a258b5b1ae14dd57c768e2e8a
Harrison Hess <harrisonhess1@aol.com>
2020-08-10 20:22:34 -0400

    lots of stuff. volunteer list is broken

---
+++ Project 0/Project 0/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Project_0.Models;
+namespace Project_0.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IVolunteerRepository repository;
+        public NavigationMenuViewComponent(IVolunteerRepository repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Volunteers
+            .Distinct()
+            .OrderBy(x => x));
+        }
+    }
+}
--- Project 0/Project 0/Startup.cs
+++ Project 0/Project 0/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Project_0.Models;
 
 namespace Project_0
 {
@@ -24,6 +25,8 @@ namespace Project_0
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMvc();
+            services.AddSingleton<IVolunteerRepository, FakeVolunteerRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
