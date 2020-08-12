34f9f3639b4def6fab8b8e9762414f657fb3eadf
Harrison Hess <harrisonhess1@aol.com>
2020-08-11 22:28:38 -0400

    various misc stuff

--- Project 0/Project 0/Controllers/AdminController.cs
+++ Project 0/Project 0/Controllers/AdminController.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Project_0.Controllers
 {
     public class AdminController : Controller
     {
-        public ViewResult Index()
+        public IActionResult Index()
         {
             return View();
         }
