f2818fe5ff55434475bfb98421ede6e2bce02127
Harrison Hess <harrisonhess1@aol.com>
2020-08-12 18:12:45 -0400

    fixed login

--- Project 0/Project 0/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Project 0/Project 0/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -66,7 +66,7 @@ namespace Project_0.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new VMSUser { UserName = Input.Email, Email = Input.Email };
+                var user = new VMSUser { UserName = Input.Email, Email = Input.Email, EmailConfirmed = true };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
