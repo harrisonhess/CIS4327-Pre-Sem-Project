253f52f9a08a198ee6a5222581cbbf9c29b021b7
Harrison Hess <harrisonhess1@aol.com>
2020-08-11 20:14:01 -0400

    misc updates and opportunity model

---
+++ Project 0/Project 0/Models/Opportunity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Project_0.Models
+{
+    public class Opportunity
+    {
+        [Required(ErrorMessage = "Please enter opportunity name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter opportunity center")]
+        public string Center { get; set; }
+        [Required(ErrorMessage = "Please enter opportunity date")]
+        public string Date { get; set; }
+    }
+}
